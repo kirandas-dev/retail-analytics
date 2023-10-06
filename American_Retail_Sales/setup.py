90ccee4bc2886a95fcd544bac2dc7f83644212bf
kirandas-dev <KiranShanker.Das@student.uts.edu.au>
2023-10-07 09:51:14 +1100

    First Commit

---
+++ American_Retail_Sales/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project involves developing two distinct predictive and forecasting models for an American retailer with 10 stores across three different states (California, Texas, and Wisconsin) that sell items from three categories: hobbies, foods, and household.',
+    author='Kiran Shanker Das',
+    license='',
+)
