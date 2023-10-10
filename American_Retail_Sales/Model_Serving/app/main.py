19898a3cf6a0df13d4b102b13ed463a1b301b218
kirandas-dev <KiranShanker.Das@student.uts.edu.au>
2023-10-10 19:57:31 +1100

    SRC Components

--- American_Retail_Sales/Model_Serving/app/main.py
+++ American_Retail_Sales/Model_Serving/app/main.py
@@ -3,7 +3,7 @@ from starlette.responses import JSONResponse
 from joblib import load
 import pandas as pd
 import os 
-from src.models.predict_model import ARIMATransformer
+from American_Retail_Sales.src.models.forecast_predict_model import ARIMATransformer
 
 models_dir = "models/forecasting"  # Update the directory name
 os.makedirs(models_dir, exist_ok=True)
@@ -35,6 +35,33 @@ def healthcheck():
 
 
 @app.get('/national', status_code=200)
+def sales_forecast(input_date: str):
+    try:
+        # Convert the input date to a pandas datetime object
+        input_date = pd.to_datetime(input_date)
+        
+        # Filter historical data up to the input date
+        historical_data_up_to_input_date = train_data[train_data['date'] <= input_date]
+        
+        # Ensure that there's enough data for forecasting
+        if len(historical_data_up_to_input_date) < 7:
+            return JSONResponse(content={"error": "Not enough historical data for forecasting."}, status_code=400)
+        
+        # Load the forecasting pipeline
+        #forecast_pipe = load('Models/Forecasting/arima_pipeline.joblib')
+        
+        
+        # Use the pipeline to make sales forecasts
+        sales_forecast = forecast_pipe.transform(historical_data_up_to_input_date['sales'])
+        print ("interim sales_forecast", sales_forecast)
+        # Return the sales forecasts as JSON
+        return JSONResponse(content={"sales_forecast": sales_forecast.tolist()}, status_code=200)
+    
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+@app.get('/stores/items/', status_code=200)
 def sales_forecast(input_date: str):
     try:
         # Convert the input date to a pandas datetime object
